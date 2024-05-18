0c6751ed37f8eea9e6f7e7e32ee7765521dea6a3
liltymoor <timur.chelter@mail.ru>
2024-05-18 13:42:23 +0300

    db script commit

--- db_init.sql
+++ db_init.sql
@@ -1,5 +1,11 @@
+/*
+    Для выполнения скрипта зайди под postgres'ом в psql
+ */
+
 CREATE USER cmd_server WITH PASSWORD '8877Temur';
 CREATE DATABASE cmd_server OWNER cmd_server;
+
+\connect cmd_server cmd_server
 
 CREATE EXTENSION pgcrypto;
 
@@ -18,24 +24,24 @@ CREATE TYPE mood AS ENUM(
     'RAGE'
     );
 
-CREATE TABLE cmd_server.humanbeing (
-    id SERIAL PRIMARY KEY,
-    name varchar(32) NOT NULL,
-    coord_x real,
-    coord_y bigint CHECK ( coord_y > -195 ) NOT NULL,
-    zoneddt varchar(64) NOT NULL,
-    realhero boolean NOT NULL,
-    hastoothpick boolean NOT NULL,
-    impactspeed bigint NOT NULL,
-    human_weapon weapontype NOT NULL,
-    human_mood mood NOT NULL,
-    carname varchar(16) NOT NULL,
-    entityowner varchar(32) NOT NULL
+CREATE TABLE humanbeing (
+                            id SERIAL PRIMARY KEY,
+                            name varchar(32) NOT NULL,
+                            coord_x real,
+                            coord_y bigint CHECK ( coord_y > -195 ) NOT NULL,
+                            zoneddt varchar(64) NOT NULL,
+                            realhero boolean NOT NULL,
+                            hastoothpick boolean NOT NULL,
+                            impactspeed bigint NOT NULL,
+                            human_weapon weapontype NOT NULL,
+                            human_mood mood NOT NULL,
+                            carname varchar(16) NOT NULL,
+                            entityowner varchar(32) NOT NULL
 );
 
-CREATE TABLE cmd_server.users (
-    id SERIAL PRIMARY KEY,
-    username varchar(32) UNIQUE,
-    passwordHash varchar(100)
+CREATE TABLE users (
+                       id SERIAL PRIMARY KEY,
+                       username varchar(32) UNIQUE,
+                       passwordHash varchar(100)
 )
 
