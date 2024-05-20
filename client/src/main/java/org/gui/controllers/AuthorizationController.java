6c5908feb55ecef9cf0c88ae47e5a1bd9a1070da
grigory <grigory.voro@gmail.com>
2024-05-20 22:15:05 +0300

    done: login&reg window (+languages)

--- client/src/main/java/org/gui/controllers/AuthorizationController.java
+++ client/src/main/java/org/gui/controllers/AuthorizationController.java
@@ -1,20 +1,20 @@
 package org.gui.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.client.ClientAppBackend;
 
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Locale;
-import java.util.PropertyResourceBundle;
+import java.util.Map;
 import java.util.ResourceBundle;
 
-public class AuthorizationController {
+public class AuthorizationController implements Initializable {
     @FXML
     private AnchorPane pane;
     @FXML
@@ -45,55 +45,101 @@ public class AuthorizationController {
     private VBox regVBox;
     @FXML
     private Label errorLabel;
+    @FXML
+    private ChoiceBox<String> langChoiceBox;
 
+    private final HashMap<String, Locale> langMap = new HashMap<>();
     private final Stage mainStage;
     private final ClientAppBackend backend;
-    private final ResourceBundle messages;
-    private final ResourceBundle errors;
+    private ResourceBundle messages;
+    private ResourceBundle errors;
+    private Locale locale;
+
+    private void initializeLangMap() {
+        langMap.put("English", new Locale("en", "US"));
+        langMap.put("Русский", new Locale("ru", "RU"));
+    }
+
+    private String getLangByLocale(Locale locale) {
+        for (Map.Entry<String, Locale> entry : langMap.entrySet()) {
+            if (entry.getValue().equals(locale)) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
 
     public AuthorizationController(ClientAppBackend backend, Stage mainStage, Locale locale) {
         this.backend = backend;
         this.mainStage = mainStage;
-        this.errors = ResourceBundle.getBundle("org/client/errors", Locale.getDefault());
-        System.out.println(errors.getBaseBundleName());
-        System.out.println(errors.getLocale());
-        System.out.println(errors.keySet());
-        this.messages = ResourceBundle.getBundle("org/client/messages", Locale.getDefault());
+        this.locale = locale;
+        this.errors = ResourceBundle.getBundle("org/client/errors", locale);
+        this.messages = ResourceBundle.getBundle("org/client/messages", locale);
+        initializeLangMap();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println(locale);
+        System.out.println(getLangByLocale(locale));
+        langChoiceBox.setValue(getLangByLocale(locale));
+        langChoiceBox.getItems().addAll(langMap.keySet());
+        langChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            Locale selectedLocale = langMap.get(newValue);
+            if (selectedLocale != null) {
+                switchLocale(selectedLocale);
+            }
+        });
+
+    }
+
+    private void switchLocale(Locale newLocale) {
+        // Загрузка новых ресурсов для выбранной локали
+        messages = ResourceBundle.getBundle("org/client/messages", newLocale);
+        errors = ResourceBundle.getBundle("org/client/errors", newLocale);
+        locale = newLocale;
+        System.out.println("SWITCHED LANGUAGE");
+
+        updateUI();
+
+        // Установка новой локали в backend (если требуется)
+        //backend.setLocale(newLocale);
     }
 
     @FXML
-    public void underlineRegisterLabel(){
+    public void underlineRegisterLabel() {
         registerLabel.setStyle("-fx-underline: true");
     }
 
     @FXML
-    public void undoUnderlineRegisterLabel(){
+    public void undoUnderlineRegisterLabel() {
         registerLabel.setStyle("-fx-underline: false");
     }
 
     @FXML
-    public void undoUnderlineAuthLabel(){
+    public void undoUnderlineAuthLabel() {
         authLabel.setStyle("-fx-underline: false");
     }
+
     @FXML
-    public void underlineAuthLabel(){
+    public void underlineAuthLabel() {
         authLabel.setStyle("-fx-underline: true");
     }
 
     @FXML
-    public void showRegisterMenu(){
+    public void showRegisterMenu() {
         regVBox.setVisible(true);
         authVBox.setVisible(false);
     }
 
     @FXML
-    public void showAuthMenu(){
+    public void showAuthMenu() {
         authVBox.setVisible(true);
         regVBox.setVisible(false);
     }
 
     @FXML
-    public void register(){
+    public void register() {
         errorLabel.setText("");
         if (registerUsernameField.getText().isEmpty() || registerPasswordField1.getText().isEmpty() || registerPasswordField2.getText().isEmpty()) {
             errorLabel.setText(errors.getString("empty_field_error"));
@@ -112,9 +158,9 @@ public class AuthorizationController {
     }
 
     @FXML
-    public void auth(){
+    public void auth() {
         errorLabel.setText("");
-        if (registerUsernameField.getText().isEmpty() || registerPasswordField1.getText().isEmpty() || registerPasswordField2.getText().isEmpty()) {
+        if (usernameField.getText().isEmpty() || passwordField.getText().isEmpty()) {
             errorLabel.setText(errors.getString("empty_field_error"));
             return;
         }
@@ -126,4 +172,18 @@ public class AuthorizationController {
         stage.close();
         mainStage.show();
     }
+
+    private void updateUI() {
+        mainLabel.setText(messages.getString("auth_main_label"));
+        usernameField.setPromptText(messages.getString("username"));
+        passwordField.setPromptText(messages.getString("password"));
+        authButton.setText(messages.getString("login_button"));
+        registerLabel.setText(messages.getString("switch_auth2reg"));
+        registerUsernameField.setPromptText(messages.getString("username"));
+        registerPasswordField1.setPromptText(messages.getString("password"));
+        registerPasswordField2.setPromptText(messages.getString("repeat_password"));
+        registerButton.setText(messages.getString("register_button"));
+        authLabel.setText(messages.getString("switch_reg2auth"));
+    }
+
 }
